cb71fca10895d1dbc26b637889d25be1c71fc766
Katya Miranda <katyamiranda03@hotmail.com>
2023-02-25 15:07:45 -0600

    se creo la estrucutura para la universidad, la variable studentsArray y la func studnets para agregar las variables al arreglo

--- Universidad.playground/Contents.swift
+++ Universidad.playground/Contents.swift
@@ -11,12 +11,31 @@ struct Students {
     var promedioGeneral: Double
 }
 
+// estructura universidad
 
-let alumno1 = Students(nombreCompleto: "Juan Garcia", edad: 18, grado: 3, salon: "A", promedioGeneral: 9.0)
+struct Universidad {
+    
+    var studentsArray:[Students] = []
+    
+    mutating func students(){
+        studentsArray.append(alumno1)
+        studentsArray.append(alumno2)
+        studentsArray.append(alumno3)
+        studentsArray.append(alumno4)
+    }
+    
+}
 
 
-let alumno2 = Students(nombreCompleto: "Karla Chavez", edad: 15, grado: 1, salon: "C", promedioGeneral: 7.5)
+// datos de los alumnos
 
-let alumno3 = Students(nombreCompleto: "Carlos Torres", edad: 16, grado: 2, salon: "A", telefono: 678795643, promedioGeneral: 8.0)
+var alumno1 = Students(nombreCompleto: "Juan Garcia", edad: 18, grado: 3, salon: "A", promedioGeneral: 9.0)
 
-let alumno4 = Students(nombreCompleto: "Sonia Rodriguez", edad: 17, grado: 3, salon: "B", telefono: 559834565, promedioGeneral: 9.5)
+
+var alumno2 = Students(nombreCompleto: "Karla Chavez", edad: 15, grado: 1, salon: "C", promedioGeneral: 7.5)
+
+var alumno3 = Students(nombreCompleto: "Carlos Torres", edad: 16, grado: 2, salon: "A", telefono: 678795643, promedioGeneral: 8.0)
+
+var alumno4 = Students(nombreCompleto: "Sonia Rodriguez", edad: 17, grado: 3, salon: "B", telefono: 559834565, promedioGeneral: 9.5)
+
+print(Universidad(studentsArray: [alumno1]))

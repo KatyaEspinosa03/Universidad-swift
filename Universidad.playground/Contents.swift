5874bc9bbc50f7ed17d3ad48afb1247056309a98
Katya Miranda <katyamiranda03@hotmail.com>
2023-02-24 18:03:06 -0600

    se agraron constantes de 5 alumnos dandole valor a cada una de las propiedades de la estructura

--- Universidad.playground/Contents.swift
+++ Universidad.playground/Contents.swift
@@ -12,3 +12,11 @@ struct Students {
 }
 
 
+let alumno1 = Students(nombreCompleto: "Juan Garcia", edad: 18, grado: 3, salon: "A", promedioGeneral: 9.0)
+
+
+let alumno2 = Students(nombreCompleto: "Karla Chavez", edad: 15, grado: 1, salon: "C", promedioGeneral: 7.5)
+
+let alumno3 = Students(nombreCompleto: "Carlos Torres", edad: 16, grado: 2, salon: "A", telefono: 678795643, promedioGeneral: 8.0)
+
+let alumno4 = Students(nombreCompleto: "Sonia Rodriguez", edad: 17, grado: 3, salon: "B", telefono: 559834565, promedioGeneral: 9.5)

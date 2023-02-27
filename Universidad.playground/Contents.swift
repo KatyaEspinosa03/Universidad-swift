bd0fbbe48324183fac1b746812484864a7020362
Katya Miranda <katyamiranda03@hotmail.com>
2023-02-26 18:34:17 -0600

    se mofidico la estructura universidad agregando funcion addstudents y se creó la funcion studentClass

--- Universidad.playground/Contents.swift
+++ Universidad.playground/Contents.swift
@@ -17,25 +17,39 @@ struct Universidad {
     
     var studentsArray:[Students] = []
     
-    mutating func students(){
-        studentsArray.append(alumno1)
-        studentsArray.append(alumno2)
-        studentsArray.append(alumno3)
-        studentsArray.append(alumno4)
+    mutating func students(student: Students) {
+        studentsArray.append(student)
+        
     }
     
+    func studentClass(salon: String) -> [Students] {
+        return []
+    }
 }
 
+var University = Universidad(studentsArray: [])
 
 // datos de los alumnos
 
 var alumno1 = Students(nombreCompleto: "Juan Garcia", edad: 18, grado: 3, salon: "A", promedioGeneral: 9.0)
 
+University.students(student: alumno1)
+
 
 var alumno2 = Students(nombreCompleto: "Karla Chavez", edad: 15, grado: 1, salon: "C", promedioGeneral: 7.5)
 
+University.students(student: alumno2)
+
 var alumno3 = Students(nombreCompleto: "Carlos Torres", edad: 16, grado: 2, salon: "A", telefono: 678795643, promedioGeneral: 8.0)
+
+University.students(student: alumno3)
 
 var alumno4 = Students(nombreCompleto: "Sonia Rodriguez", edad: 17, grado: 3, salon: "B", telefono: 559834565, promedioGeneral: 9.5)
 
-print(Universidad(studentsArray: [alumno1]))
+University.students(student: alumno4)
+
+print(University.studentsArray[0])
+
+University.studentClass(salon: "A")
+
+print(University.studentClass(salon: "A"))
